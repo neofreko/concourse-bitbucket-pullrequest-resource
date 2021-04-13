ac5b8a57e215343a8717162b0440e2b26b9d447c
Akhmad Fathonih <akhmad.fathonih@rakuten.com>
2021-04-13 12:40:07 +0900

    check will use API instead of git ls-remote

--- assets/helpers/bitbucket.sh
+++ assets/helpers/bitbucket.sh
@@ -94,8 +94,20 @@ bitbucket_request() {
     exit 1
   fi
 
+  # debug
+  log "Bitbucket request ($request_url) response: $(cat $request_result)"
   # cleanup
   request_result_cleanup
+}
+
+bitbucket_pullrequests() {
+  # $1: host
+  # $2: project
+  # $3: repository id
+  # $5: netrc file (default: $HOME/.netrc)
+  # $6: skip ssl verification
+  log "Retrieving pull request #$4 for $2/$3"
+  bitbucket_request "$1" "projects/$2/repos/$3/pull-requests" "" "" "" "$6" "$5"
 }
 
 bitbucket_pullrequest() {
